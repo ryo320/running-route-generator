0ac9bc4842acbb662cf790a1ce3dcdc7fb781443
ryo320 <95510lbs@gmail.com>
2026-02-14 15:06:46 +0900

    言語設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // サイトマップの設定を追加
+    Sitemap({
+      hostname: 'https://runrout.vercel.app', // あなたのVercelのURLに変更してください
+      dynamicRoutes: ['/'], // 現時点ではトップページのみでOK
+    }),
+  ],
+  // Vercelで公開する場合は base: '/' にすることをお忘れなく！
   base: '/',
 })
